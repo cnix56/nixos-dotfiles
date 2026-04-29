9e445131a5346db68765b1145b984b0ad1f56d4a
cnix56 <268737461+cnix56@users.noreply.github.com>
2026-04-29 18:59:36 +0200

    vramfix

--- modules/nixos/vramfix.nix
+++ modules/nixos/vramfix.nix
@@ -5,41 +5,39 @@
   ...
 }:
 
-
 {
   environment.etc."nvidia/nvidia-application-profiles-rc.d/50-limit-free-buffer-pool-in-wayland-compositors.json".text =
-    builtins.toJSON {
-      rules = [
-        {
-          pattern = {
-            feature = "procname";
-            matches = "niri";
-          };
-          profile = "Limit Free Buffer Pool On Wayland Compositors";
-        }
-        {
-          pattern = {
-            feature = "procname";
-            matches = "hyprland";
-          };
-          profile = "Limit Free Buffer Pool On Wayland Compositors";
-        }
-      ];
+    ''
+          {
+          "rules": [
+              {
+                  "pattern": {
+                      "feature": "procname",
+                      "matches": "niri"
+                  },
+                  "profile": "Limit Free Buffer Pool On Wayland Compositors"
+              },
+              {
+                  "pattern": {
+                      "feature": "procname",
+                      "matches": "hyprland"
+                  },
+                  "profile": "Limit Free Buffer Pool On Wayland Compositors"
+              }
+          ],
+          "profiles": [
+              {
+                  "name": "Limit Free Buffer Pool On Wayland Compositors",
+                  "settings": [
+                      {
+                          "key": "GLVidHeapReuseRatio",
+                          "value": 0
+                      }
+                  ]
+              }
+          ]
+      }
 
-      profiles = [
-        {
-          name = "Limit Free Buffer Pool On Wayland Compositors";
-          settings = [
-            {
-              key = "GLVidHeapReuseRatio";
-              value = 0;
-            }
-          ];
-        }
-      ];
-    };
+    '';
+
 }
-
-
-
-
