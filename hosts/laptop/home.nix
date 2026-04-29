8fec336ab7cd23ea78d665f775c9fddebac8cd4d
cnix56 <268737461+cnix56@users.noreply.github.com>
2026-04-29 18:35:21 +0200

    add cursor

--- hosts/laptop/home.nix
+++ hosts/laptop/home.nix
@@ -30,7 +30,14 @@ in
 
   ];
 
-  
+    home.pointerCursor = {
+    package = pkgs.bibata-cursors;
+    name = "Bibata-Modern-Classic";
+    size = 24;
+    gtk.enable = true;
+    x11.enable = true;
+  };
+
 
   programs.zsh = {
     enable = true;
