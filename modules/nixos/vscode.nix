e663de14ad42b14c3b9243118090c6127d2add03
cnix56 <268737461+cnix56@users.noreply.github.com>
2026-05-07 00:46:10 +0200

    latest

--- modules/nixos/vscode.nix
+++ modules/nixos/vscode.nix
@@ -11,6 +11,14 @@ environment.systemPackages = with pkgs; [
     vscode = vscodium;
     vscodeExtensions = with vscode-extensions; [
       jnoortheen.nix-ide
+
+    ] ++ pkgs.vscode-utils.extensionsFromVscodeMarketplace [
+      {
+        name = "dms-theme";
+        publisher = "DankLinux";
+        version = "0.0.3";
+        sha256 = "sha256-MI1x1wiqvwg/N89oMuNVp0qlRT84ubvuMjtpkX0WKQY=";
+      }
     ];
   })
 ];
